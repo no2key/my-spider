77304f35908f740f5b1407f95fe1ad54a5bcb449
liuxd <liuxidong1984@gmail.com>
2012-12-03 17:34:05 +0800

    成功获得配置信息。

--- wall-e.c
+++ wall-e.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <curl/curl.h>
 
 int main(int argc, char *argv[]){
@@ -7,15 +10,24 @@ int main(int argc, char *argv[]){
     char cfg_line[100];
     char cfg_key[50];
     char cfg_value[100];
-    FILE *fp;
+    char *task, *queue_host, *queue_port;
+    int fp;
 
-    fp = fopen(cfg_file, "r");
+    fp = open(cfg_file, O_RDONLY);
+    read(fp, cfg_info, 1024);
 
-    while(fgets(cfg_line, 100, fp) != NULL){
-        printf("%s\n", cfg_line);
-    }
+    str_replace(cfg_info, "task", "");
+    str_replace(cfg_info, "queue_host", "");
+    str_replace(cfg_info, "queue_port", "");
+    str_replace(cfg_info, "\n", "");
 
-    fclose(fp);
+    task = strtok(cfg_info, "=");
+    queue_host = strtok(NULL, "=");
+    queue_port = strtok(NULL, "=");
+
+    printf("%s\n", task);
+    printf("%s\n", queue_host);
+    printf("%s\n", queue_port);
 
     return 0;
 }
@@ -40,5 +52,32 @@ int fetch_data(char[200] url){
        }
 }
 */
+int str_replace(char* str,char* str_src, char* str_des){
+    char *ptr=NULL;
+    char buff[256];
+    char buff2[256];
+    int i = 0;
+
+    if(str != NULL){
+        strcpy(buff2, str);
+    }else{
+        printf("str_replace err!\n");
+        return -1;
+    }
+
+    memset(buff, 0x00, sizeof(buff));
+
+    while((ptr = strstr( buff2, str_src)) !=0){
+        if(ptr-buff2 != 0) memcpy(&buff[i], buff2, ptr - buff2);
+        memcpy(&buff[i + ptr - buff2], str_des, strlen(str_des));
+        i += ptr - buff2 + strlen(str_des);
+        strcpy(buff2, ptr + strlen(str_src));
+    }
+
+    strcat(buff,buff2);
+    strcpy(str,buff);
+
+    return 0;
+}
 
 // end of this file
